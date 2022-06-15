0566af30f31ab30a6d5afb87c6787eddd23665ff
programmeranish <programmer.anish.shrestha@gmail.com>
2022-06-15 16:56:15 +0545

    initial

---
+++ static/assests/js/bird.js
@@ -0,0 +1,11 @@
+let backGroundContainer = document.getElementsByClassName(
+  "background-container"
+)[0];
+class Bird {
+  constructor() {
+    this.bird = document.createElement("div");
+    this.bird.style.height = "200px";
+    this.bird.style.width = "200px";
+    this.bird.style.position = "absolute";
+  }
+}
--- static/assests/js/utils.js
+++ static/assests/js/utils.js
@@ -1,3 +1,8 @@
 function getRandomNumber(min, max) {
   return `${Math.floor(Math.random() * (min - max)) + min}`;
 }
+
+function toPixel(number) {
+  return `${number}px`;
+}
+export { getRandomNumber, toPixel };
